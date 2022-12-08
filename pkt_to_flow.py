42b94fbfbccbe126fc1fa2a75cdf053ffbd07dfd
jragsdale <jbrags42@gmail.com>
2022-12-07 18:09:14 -0600

    Cleanup

--- pkt_to_flow.py
+++ pkt_to_flow.py
@@ -6,13 +6,11 @@ def main():
     t = read_pkts(interface='eno1', filename='test.csv')
 
     t.start()
-
     i = 0
     while i < 75:
         print(flows)
         i += 1
         time.sleep(1)
-
     # TODO: IF ALERT, LABEL MALICIOUS
     t.stop()
 
